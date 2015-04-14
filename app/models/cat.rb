13abccc1f6b70c67c0a591c0afc6ed76fc564381
rhoen+jeremy <rhoen+jeremy@appacademy.io>
2015-04-14 15:38:43 -0400

    a lot. edit, create cats.

--- app/models/cat.rb
+++ app/models/cat.rb
@@ -1,2 +1,33 @@
 class Cat < ActiveRecord::Base
+  validates :name, presence: true
+  validate :check_color
+
+  COLORS = ["black", "calico", "incarnadine", "seafoam", "lazuli", "space grey", "violet", "jade green"]
+
+
+  def cat_colors
+    COLORS
+  end
+
+
+  def age
+    ((Date.today - birth_date).to_i / 365)
+  end
+
+  def birthday
+    birth_date.strftime("%B %d, %Y")
+  end
+
+
+  private
+    def check_color
+     unless COLORS.include?(self.color)
+       errors[:color] << "That's not a color..."
+     end
+    end
+
+
+
+
+
 end
